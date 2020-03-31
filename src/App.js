77b4981ba9efa8b8cdd19b3e877ae843fef12cec
zhaoxunyin <17603016347@163.com>
2020-03-31 15:26:47 +0800

    提交webpack配置

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import {Button} from "antd"
 import './App.css';
 
 function App() {
@@ -8,6 +8,7 @@ function App() {
 			<p>你是一个中国人民吗?</p>
 			<p>我爱我的媳妇,我想她了,她在哪呢?</p>
 			<p>大哥我猜你不是一般人啊,真牛逼,龙哥!</p>
+        <Button type='danger'>我的祖国</Button>
     </div>
   );
 }
