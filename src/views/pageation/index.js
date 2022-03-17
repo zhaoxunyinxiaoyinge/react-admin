05196f924fedaa85793bc21d1c387f426666b68c
zhaoxunyin <17603016347@163.com>
2022-03-17 15:49:29 +0800

    提交

--- src/views/pageation/index.js
+++ src/views/pageation/index.js
@@ -1,12 +1,49 @@
-import React,{Component} from "react"
+import React, { Component } from "react";
 
-const Pageation=class extends Component {
-    constructor(){
-        super();
+const Pageation = class extends Component {
+  constructor() {
+    super();
+  }
+  
+  render() {
+    const { x, y } = this.props;
+    return (
+      <div>
+        {/* <img  style={{position:"absolute",left:x-20,top:y-20}}
+         src="https://files.jb51.net/image/8a210.gif"></img> */}
+      </div>
+    );
+  }
+};
+
+function createPagetions(Wouter) {
+  class mouse extends Component {
+    constructor() {
+      super();
+      this.state = {
+        x: 0,
+        y: 0,
+      };
     }
-    render(){
-        return (<div>12345</div>)
+
+    componentDidMount() {
+        this.mouse();
     }
+
+    mouse() {
+    }
+
+    render() {
+      return <Wouter {...this.state}></Wouter>;
+     }
+   } 
+   mouse.displayName=`mouse${getDisplayName(Wouter)}`
+  return mouse
 }
 
-export default  Pageation
+
+function getDisplayName(component){
+    return component.displayName||component.name||'component'
+}
+
+export default createPagetions( Pageation);
--- src/views/roles/index.js
+++ src/views/roles/index.js
@@ -1,11 +1,25 @@
 import React,{Component} from "react"
 
+const Child=(props)=>{
+    const {getUserName}=props;
+    getUserName("zhaoxuinyin")
+    return <div>11111</div>
+}
+
+
 const Roles=class extends Component {
     constructor(){
         super();
     }
+    getusername(res){
+        console.log(res);
+    }
+
     render(){
-        return (<div>12345</div>)
+        const {getusername}=this
+        return (<div>
+            <Child getUserName={getusername}></Child>
+        </div>)
     }
 }
 
